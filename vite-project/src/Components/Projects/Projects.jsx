a0c52d1d094cc89fbfa2cb6c6856bce0d6d18ef9
Jerson Gonzalez <jersgonz310@outlook.com>
2023-07-27 19:52:52 -0500

    Projects, faltan fotos y terminar estilos

--- vite-project/src/Components/Projects/Projects.jsx
+++ vite-project/src/Components/Projects/Projects.jsx
@@ -1,30 +1,81 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Projects = () => {
-  // Aquí puedes reemplazar los datos de ejemplo con tus propios proyectos
-  const projectsData = [
-    {
-      title: 'Proyecto 1',
-      description: 'Breve descripción del proyecto 1.',
-    },
-    {
-      title: 'Proyecto 2',
-      description: 'Breve descripción del proyecto 2.',
-    },
-    // Agrega más proyectos aquí si es necesario
-  ];
+    const projectsData = [
+        {
+            title: 'HenryCollege',
+            description: 'Breve descripción del proyecto 1.',
+            images: [
+                '/path/to/your/project1-image1.jpg', '/path/to/your/project1-image2.jpg', '/path/to/your/project1-image3.jpg',
+            ],
+            link: 'https://www.example.com/project1'
+        }, {
+            title: 'FarmaStack',
+            description: 'Breve descripción del proyecto 2.',
+            images: [
+                'https://i.ibb.co/3k5jR3w/zyro-image.png', 'https://i.ibb.co/bN2wbwM/Captura-de-pantalla-2023-05-23-162058.png'
+            ],
+            link: 'https://www.example.com/project2'
+        },
+        // Agrega más proyectos aquí si es necesario
+    ];
 
-  return (
-    <section id='projects' className="p-4">
-      <h2 className="text-2xl font-bold mb-2">Projects</h2>
-      {projectsData.map((project, index) => (
-        <div key={index} className="mb-4">
-          <h3 className="text-xl font-bold">{project.title}</h3>
-          <p className="text-gray-400">{project.description}</p>
-        </div>
-      ))}
-    </section>
-  );
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    return (
+        <section id='projects' className="p-4">
+            <h2 className="text-2xl text-center text-gray-200 font-extrabold sm:text-3xl md:text-4xl  my-20 mb-5">Projects</h2>
+            <div className="grid grid-cols-2 gap-4">
+                {
+                projectsData.map((project, index) => (
+                    <div key={index}
+                        className="bg-slate-800 p-4 rounded-lg shadow-md">
+                        <Slider {...sliderSettings}>
+                            {
+                            project.images.map((image, imageIndex) => (
+                                <div key={imageIndex}>
+                                    <img src={image}
+                                        alt={
+                                            `${
+                                                project.title
+                                            } - Image ${
+                                                imageIndex + 1
+                                            }`
+                                        }
+                                        className="w-screen h-screen object-scale-down mb-4 rounded-lg"/>
+                                </div>
+                            ))
+                        } </Slider>
+                        <h3 className="text-xl text-gray-100 font-bold mb-2">
+                            {
+                            project.title
+                        }</h3>
+                        <p className="text-gray-400 mb-4">
+                            {
+                            project.description
+                        }</p>
+                        <a href={
+                                project.link
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="block w-full bg-blue-500 text-white text-center py-2 rounded-md hover:bg-blue-600">
+                            Ver más
+                        </a>
+                    </div>
+                ))
+            } </div>
+        </section>
+    );
 };
 
 export default Projects;
